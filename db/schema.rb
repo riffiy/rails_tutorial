86a36b0eecbb6486acbffec473bf89444e98890d
riffiy <riffiy.6135@gmail.com>
2023-04-25 07:40:14 +0000

    rails_tutorial_2.4.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_24_104509) do
+ActiveRecord::Schema.define(version: 2023_04_25_072139) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
